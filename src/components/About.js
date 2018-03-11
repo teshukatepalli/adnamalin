73b63119fb8f81081c6a832895484e0d09c377f8
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2018-03-11 00:30:01 -0600

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -8,9 +8,9 @@ const About = () => {
         className="About-image"
         alt="amanda"/>
       <div className="text">
-        When I'm not thinking about what to build next, there's a 99% chance I'm thinking about what I'm going to eat next. I love food and learning about food culture. In my free time you'll find me binging documentaries on netflix and chilling with my pup Ein aka Einstein.
+        When I'm not thinking about what to build next, there's a 99% chance I'm thinking about what I'm going to eat next. I love food and learning about food culture. In my free time you'll find me binging documentaries on netflix and chilling with my pups Ein & Gohan.
         <br /><br />
-        I'm Ohio born and raised but currently living in Chicago. While I'm midwestern at heart, I am eager to move to the LA area and eventually live & work abroad.
+        I'm Ohio born and raised but currently living in Chicago. While I'm midwestern at heart, I am eager to move to California.
       </div>
     </div>
   )
